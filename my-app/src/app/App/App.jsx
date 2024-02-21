25dd28adb1734b060af1c46eae9cf15f10861796
Nikolaihait4 <jet1nikolai@gmail.com>
2024-02-21 08:34:51 +0100

    todos_list

---
+++ my-app/src/app/App/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState(" ");
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <label htmlFor="">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span style={{ color: "red" }}>&times;</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
